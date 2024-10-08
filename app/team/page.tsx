0977707e5916be8c0443925987fa806eb29026c6
Kigen29 <emmanuel.kemboi@strathmore.edu>
2024-10-08 16:55:13 +0300

    feat: add team and about us

---
+++ app/team/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Team = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Details about your law firm...</p>
+    </div>
+  );
+};
+
+export default Team;
